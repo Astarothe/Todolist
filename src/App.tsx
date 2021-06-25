49e777fd4361875035769a4842c451a3df61a2c5
Astaroth <minatosavich@gmail.com>
2021-06-25 14:52:51 +0300

    fix: refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,6 @@ type TasksType = {
 }
 
 function App() {
-    // let [tasks, setTasks] = useState([
-    //     {id: v1(), title: "HTML&CSS", isDone: true},
-    //     {id: v1(), title: "JS", isDone: true},
-    //     {id: v1(), title: "ReactJS", isDone: false},
-    //     {id: v1(), title: "Rest API", isDone: false},
-    //     {id: v1(), title: "GraphQL", isDone: false},
-    // ]);
-
     let todolistId1 = v1();
     let todolistId2 = v1();
 
